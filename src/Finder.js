66d0773b25bcd1b970804fe36de5faf5763f7533
alex <alexmir2015@yandex.ru>
2019-11-21 00:22:33 +0300

    show text 100 symbols over word

--- src/Finder.js
+++ src/Finder.js
@@ -15,30 +15,48 @@ class Finder extends React.Component {
 
     handleChange(event) {
         console.log(this.check);
-        if(this.check===true) this.find(event.target.value);
+        if (this.check === true) this.find(event.target.value);
         this.setState({text: event.target.value});
     }
 
-    async find(string){
+    async find(string) {
         let words = string.trim().replace(/\s/g, ',').replace(/\s/g, '');
-        await fetch('/api/invertedindex?words=' + words).then(async data => this.setState({texts: await data.json()}));
+        let words1 = string.trim().split(/\s/g);
+        await fetch('/api/invertedindex?words=' + words).then(async data => {
+            var texts = await data.json();
+            var trimText = texts.map(text=>text.text).flatMap(text => this.getMatches(words1[0], text).map(x => text.substring(x - 100, x + 100)));
+            console.log(trimText);
+            this.setState({texts: trimText})
+        });
     }
 
     async handleFind() {
         await this.find(this.state.text);
     }
 
+    getMatches(needle, haystack) {
+        var myRe = new RegExp("\\b" + needle + "\\b((?!\\W(?=\\w))|(?=\\s))", "gi"),
+            myArray, myResult = [];
+        while ((myArray = myRe.exec(haystack)) !== null) {
+            myResult.push(myArray.index);
+        }
+        return myResult;
+    }
+
     render() {
         let texts = this.state.texts;
-        let list = texts.map(text => <li><p align="justify">{text.text}</p></li>)
+        let list = texts.map(text => <li><p align="justify">{text}</p></li>)
         return <div class="text">
             <h3>Простой поисковик</h3>
             <InputGroup>
-                <Input type="text" name="find" id="find" placeholder="Введите слово, например IT" onChange={this.handleChange}/>
+                <Input type="text" name="find" id="find" placeholder="Введите слово, например IT"
+                       onChange={this.handleChange}/>
                 <Button className={'but'} onClick={this.handleFind} color="danger">Find</Button>
             </InputGroup>
-                <Input type="checkbox" onChange={(e)=>{this.check=!this.check;}} type="checkbox" />{' '}
-                Поиск при изменении текста
+            <Input type="checkbox" onChange={(e) => {
+                this.check = !this.check;
+            }} type="checkbox"/>{' '}
+            Поиск при изменении текста
             <ul>
                 {list}
             </ul>
